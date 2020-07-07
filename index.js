120d6292852508fec8add766f43eb073e6794475
Ivan Leung <ivanleung1@gmail.com>
2020-07-06 23:37:28 -0700

    Initial commit for basic bot to respond to ping

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+const Discord = require("discord.js")
+const client = new Discord.Client()
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!")
+  }
+})
+client.login(process.env.BOT_TOKEN)
