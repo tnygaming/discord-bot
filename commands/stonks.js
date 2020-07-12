28db3dbe552a58843858a38e5ab6026fb6e3bd6c
Ivan Leung <ivanleung1@gmail.com>
2020-07-12 02:44:47 -0700

    Include better error handling and fix hardcoded value

--- commands/stonks.js
+++ commands/stonks.js
@@ -7,20 +7,27 @@ api_key.apiKey = config.finnhub_api_key;
 const finnhubClient = new finnhub.DefaultApi();
 
 exports.run = async (client, message, args, level) => {
-  console.log(args);
-  const company = args[0];
-  console.log(company);
-
-  if(company) {
-    finnhubClient.quote("VEEV", (error, data, response) => {
+  if(args[0]) {
+    const company = args[0].toUpperCase();
+    finnhubClient.quote(company, (error, data, response) => {
+      console.log(company);
       console.log(data);
-      message.channel.send(`\`\`\`\n
+      if(Object.keys(data).length === 0) {
+        message.reply("Invalid ticker ["+company+"]");
+      } else {
+        message.channel.send(`\`\`\`\n
+Company: ${company}
+
 Today's Open: ${data.o}
 Today's High: ${data.h}
 Today's Low: ${data.l}
 Current Price: ${data.c}
 Yesterday's Close: ${data.pc}\`\`\``);
+      }
     });
+      
+  } else {
+    message.reply(".stonks [ticker]")
   }
 
 };
@@ -29,12 +36,12 @@ exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: [],
-  permLevel: "Users"
+  permLevel: "User"
 };
 
 exports.help = {
   name: "stonks",
   category: "Miscelaneous",
   description: "Stonks only go up",
-  usage: "stonks <ticker>"
+  usage: "stonks [ticker]"
 };
