fc4df972698fec3a86eb8942c6c70b17044e443c
Brandon Lee <brandon.lee@veeva.com>
2020-07-14 22:54:30 -0700

    Add an isitracist command. Update config.js.example with newer fields

---
+++ commands/isitracist.js
@@ -0,0 +1,24 @@
+const config = require("../config.js");
+const default_choices = ["Yes", "No", "Maybe", "¯\\_(ツ)_/¯"];
+
+exports.run = async (client, message, args, level) => { 
+  choices = []
+  choices.push(...config.is_it_racist_choices);
+  choices.push(...default_choices);
+  const num = Math.floor(Math.random() * Math.floor(choices.length))
+  const msg = await message.channel.send(choices[num]);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "isitracist",
+  category: "Miscelaneous",
+  description: "Is it racist?",
+  usage: "isitracist"
+};
