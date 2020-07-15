775bc56edfebc9bc0e699cb37ed8e6822ac03cea
Brandon Lee <brandon.lee@veeva.com>
2020-07-15 00:47:27 -0700

    Default to using default choices if nothing configured for isitracist

--- commands/isitracist.js
+++ commands/isitracist.js
@@ -2,9 +2,13 @@ const config = require("../config.js");
 const default_choices = ["Yes", "No", "Maybe", "¯\\_(ツ)_/¯"];
 
 exports.run = async (client, message, args, level) => { 
-  choices = []
-  choices.push(...config.is_it_racist_choices);
-  choices.push(...default_choices);
+  choices = null
+  if (config.is_it_racist_choices == undefined || config.is_it_racist_choices.length == 0) {
+    choices = default_choices;
+  } else {
+    choices = config.is_it_racist_choices;
+  }
+
   const msg = await message.channel.send(choices.random());
 };
 
