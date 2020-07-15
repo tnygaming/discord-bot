6253acef8da7905d6181ec1de4a374a4c428d28c
Brandon Lee <brandon.lee@veeva.com>
2020-07-14 23:00:37 -0700

    Remove unnecessary Math.floor call
    Copy and pasted it from somewhere :)

--- commands/isitracist.js
+++ commands/isitracist.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args, level) => {
   choices = []
   choices.push(...config.is_it_racist_choices);
   choices.push(...default_choices);
-  const num = Math.floor(Math.random() * Math.floor(choices.length))
+  const num = Math.floor(Math.random() * choices.length)
   const msg = await message.channel.send(choices[num]);
 };
 
