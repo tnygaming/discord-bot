0ad25106803fcc4e2b43e5af018972ab3f2ab568
andi-n <37681586+andi-n@users.noreply.github.com>
2021-01-11 15:10:51 -0800

    finished up adding the roles and emojis

--- modules/roleReaction.js
+++ modules/roleReaction.js
@@ -7,16 +7,21 @@ module.exports = async (client) => {
         client.emojis.cache.find((emoji) => emoji.name === emojiName)
 
     const emojis = {
-        fuckValorant: 'CS:GO',
-        gotRice: 'Tarkov',
-        smokeTrees: 'Apex',
-        passTheAWP: 'Ivan'
+        counterstrike: 'csgo',
+        apex: 'apex',
+        dota2: 'tnydotatoes',
+        valorant: 'valorant',
+        tarkov: 'tarkov',
+        ghostbusters: 'ghostbusters',
+        gunbound: 'gunbound',
+        mmabois: 'mmabois',
+        l4dtime: 'l4dtime'
     }
 
     const reactions = []
 
     //TODO: maybe add this as an embed
-    let emojiText = 'Welcome to TNY!\nWe are chill group of homies who suck at games.\nYou\'ll usually catch us playing our main squeeze: Counter-Strike, but we also play other FPS games and party games!\n\nThese roles are focused on the many games we play in this discord.\nPlease react to the corresponding role and you will be added to that game\'s role and will be notified with an \'@\' in the chat when anyone is looking for a party.\nPlease remember use the \'@\' symbol followed by the game role when you are looking for a party!\n\n'
+    let emojiText = 'Welcome to TNY! We are chill group of homies who suck at games.\n\nYou\'ll usually catch us playing our main squeeze: Counter-Strike, but we also play other FPS games and party games!\nThese roles are focused on the many games and other hobbies we play/watch in this discord.\nPlease remember use the \'@\' symbol followed by the game role when you are looking for a party!\n\nReact to the corresponding role and you will be added to that game\'s role and will be notified with an \'@\' in the chat when anyone is looking for a party.\n'
     for (const key in emojis) {
         const emoji = getEmoji(key)
         reactions.push(emoji)
