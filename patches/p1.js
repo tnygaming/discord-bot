fa6ca29c477d271e8cc65ad5b564fe02bf40190d
tianhongtan <tianhongtan@gmail.com>
2021-01-31 10:16:34 -0800

    remove patch manual test code

--- patches/p1.js
+++ patches/p1.js
@@ -6,11 +6,6 @@ const v2Roles = new Enmap({name :'rolesDataV2'});
  * Migration for the roles command's schema
  */
 function migrateRolesToV2() {
-  v1Roles.set('143146071814569986', [
-    "tarkov",
-    "tnydotatoes",
-    "theboiis"]);
-
   console.log(`rolesV1-pre: ${v1Roles.export()}`);
   console.log(`rolesV2-pre: ${v2Roles.export()}`);
 
