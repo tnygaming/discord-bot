84797703d7cf83ff29594e1e411c93c89ae66eec
tianhongtan <tianhongtan@gmail.com>
2021-01-31 21:17:02 -0800

    Introduce a patching framework to handle updating roles backing data (#90)
    * Introduce a patching framework to handle updating roles backing data
    
    * remove patch manual test code
    
    * remove patch manual test code

---
+++ modules/Patcher.js
@@ -0,0 +1,40 @@
+const Enmap = require("enmap");
+const patchHistory = new Enmap({name: 'patches'})
+const readdir = require("fs").readdirSync;
+
+/**
+ * Applies any patches (i.e. 1 time run commands), ensuring that they run successfully.
+ * Please make sure that your patches are idempotent. 
+ */
+function applyPatches() {
+  console.log(`patches begin`);
+  //readdir uses process.cwd instead of __dirname, so use root dir as rel path.
+  const patches = readdir("./patches"); 
+  patches.sort(function(a,b){return a.substring(1) - b.substring(1)});
+  patches.forEach(patch => {
+    ensurePatch(patch);
+  });  
+  console.log(`patches end`);
+}
+
+function ensurePatch(patchName) {
+  if(!patchHistory.ensure(patchName, false)) {
+    console.log(`Applying patch ${patchName}`);
+    const patch = require(`../patches/${patchName}`);
+    try{
+      patch.run();
+    } catch (e) {
+      console.log(`${patchName} failed to apply, rolling back`)
+      patch.rollback();
+      console.log(`${patchName} rolled back`)
+      throw e;
+    }
+    patchHistory.set(patchName, true);
+    console.log(`${patchName} applied successfully`);  
+  }
+  else {
+    console.log(`${patchName} already applied`);
+  }
+}
+
+module.exports.applyPatches = applyPatches
