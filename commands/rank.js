8175cecbeac6569b257f5a32cdcc255ff851545a
Brandon Lee <bernardoodoo@gmail.com>
2021-04-05 19:41:26 -0700

    Update rank.js
    update wording cuz ivan boss

--- commands/rank.js
+++ commands/rank.js
@@ -54,9 +54,9 @@ exports.run = async (client, message, args, level) => {
        // retrieve data for user
         const oldRank = client.ranks.get(key);
         if (oldRank) {
-          channel.send(`${message.author.username}'s rank has been set to: ${rank}. Was ${oldRank}`);
+          channel.send(`${message.author.username}'s rank was updated from ${oldRank} to ${rank}`);
         } else {
-          channel.send(`${message.author.username}'s rank has been set to: ${rank}`);
+          channel.send(`${message.author.username}'s rank was set to ${rank}`);
         }
       }
 
